6fbd6200776072e2ba3eaf0069e5e3ca8a874cf9
rocio picaza <rocio.picaza@gmail.com>
2021-04-16 12:48:41 -0300

    app index

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">hola</div>
+        <div className="right-scene">right</div>
+      </div>
+    );
+  }
+}
+
+export default App;
