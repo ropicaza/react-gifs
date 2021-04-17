f075f152912845c2d51e0365b227cf5591f5b4df
rocio picaza <rocio.picaza@gmail.com>
2021-04-17 17:40:23 -0300

    my first react app is working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import giphy from 'giphy-api';
 
 import SearchBar from './search_bar';
-import Gif from './gif.jsx';
+import Gif from './gif';
 import GifList from './gif_list';
+
+const GIPHY_APP_KEY = '12DrxtISS76ZFBiYGvHa8why0AEBpiFv'
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   constructor(props) {
-    super(props); 
+    super(props);
 
     this.state = {
       gifs: [],
-      selectedGifId: "dPIPDAF8hOlZC"
-    }
+      selectedGifId: "dPIPDAF8hOlZC",
+    };
 
-    this.search("disney");
+    this.search = this.search.bind(this);
+    this.selectGif = this.selectGif.bind(this);
   }
 
   search = (query) => {
-    giphy('12DrxtISS76ZFBiYGvHa8why0AEBpiFv').search({
+    giphy(GIPHY_APP_KEY).search({
       q: query,
       rating: 'g',
       limit: 10
@@ -31,17 +34,24 @@ class App extends Component {
 
   }
 
+  selectGif(id) {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id={ this.state.selectedGifId }/>
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
     );
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
   render() {
     const src = `https://media1.giphy.com/media/${this.props.id}/giphy.gif?cid=ecf05e472v6b2p8bgiiw8v8p71qd2kiahwlki8c7low1iehz&rid=giphy.gif&ct=g`;
     return (
-      <img src={src} alt="" className="gif"/>
+      <img src={src} alt="" className="gif" onClick={this.handleClick}/>
     );
   }
 }
